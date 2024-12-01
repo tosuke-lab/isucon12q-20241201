027c6b89ac7fb0b800ad8a74e477afa1096735d2
tosuke <13393900+tosuke@users.noreply.github.com>
2024-12-02 07:09:22 +0900

    init delete billing

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,6 @@
 DELETE FROM tenant WHERE id > 100;
 DELETE FROM visit_history WHERE created_at >= '1654041600';
 DELETE FROM first_visit WHERE created_at >= '1654041600';
+DELETE FROM billing_report;
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
