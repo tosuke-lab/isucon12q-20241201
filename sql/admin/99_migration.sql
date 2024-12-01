e65ed3c3b947c33dc6614a9c653be08e5b94cbfd
tosuke <13393900+tosuke@users.noreply.github.com>
2024-12-02 06:48:07 +0900

    add billing_report

--- sql/admin/99_migration.sql
+++ sql/admin/99_migration.sql
@@ -1,6 +1,7 @@
 USE `isuports`;
 
 DROP TABLE IF EXISTS `first_visit`;
+DROP TABLE IF EXISTS `billing_report`;
 
 CREATE TABLE `first_visit` (
   `player_id` VARCHAR(255) NOT NULL,
@@ -15,3 +16,16 @@ INSERT INTO first_visit(player_id, tenant_id, competition_id, created_at)
 SELECT player_id, tenant_id, competition_id, MIN(created_at) AS created_at
 FROM visit_history
 GROUP BY player_id, tenant_id, competition_id;
+
+CREATE TABLE `billing_report` (
+	`tenant_id` BIGINT UNSIGNED NOT NULL,
+	`competition_id` VARCHAR(255) NOT NULL,
+	`competition_title` TEXT NOT NULL,
+	`player_count` BIGINT NOT NULL,
+	`visiter_count` BIGINT NOT NULL,
+	`billing_player_yen` BIGINT NOT NULL,
+	`billing_visiter_yen` BIGINT NOT NULL,
+	`billing_yen` BIGINT NOT NULL,
+	INDEX `tenant_id_idx` (`tenant_id`),
+	UNIQUE KEY `tenant_competition` (`tenant_id`, `competition_id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
