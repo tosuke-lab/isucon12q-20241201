66cdd986426cbb4eedf2b8c39e074fa592c4989f
tosuke <13393900+tosuke@users.noreply.github.com>
2024-12-02 01:22:29 +0900

    visit once

--- go/isuports.go
+++ go/isuports.go
@@ -1277,8 +1277,8 @@ func competitionRankingHandler(c echo.Context) error {
 
 	if _, err := adminDB.ExecContext(
 		ctx,
-		"INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
-		v.playerID, tenant.ID, competitionID, now, now,
+		`INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?) WHERE NOT EXISTS (SELECT 1 WHERE player_id = ? AND tenant_id = ? AND competition_id = ?)`,
+		v.playerID, tenant.ID, competitionID, now, now, v.playerID, tenant.ID, competitionID,
 	); err != nil {
 		return fmt.Errorf(
 			"error Insert visit_history: playerID=%s, tenantID=%d, competitionID=%s, createdAt=%d, updatedAt=%d, %w",
--- sql/admin/10_schema.sql
+++ sql/admin/10_schema.sql
@@ -28,4 +28,5 @@ CREATE TABLE `visit_history` (
   `created_at` BIGINT NOT NULL,
   `updated_at` BIGINT NOT NULL,
   INDEX `tenant_id_idx` (`tenant_id`)
+  INDEX `player_tenant_competition_idx` (`player_id`, `tenant_id`, `competition_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
