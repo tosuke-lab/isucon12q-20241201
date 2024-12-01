8a3e9263e74cc75f3b603abb33675f11c953d817
isucon <isucon@example.com>
2024-12-01 13:14:01 +0000

    add nginx

--- deploy-local.sh
+++ deploy-local.sh
@@ -3,6 +3,8 @@
 set -xe
 
 sudo cp conf/isuports-app.service /etc/systemd/system && sudo systemctl daemon-reload
+sudo cp conf/nginx.conf /etc/nginx/nginx.conf
+sudo cp conf/nginx-isuports.conf /etc/nginx/sites-available/isuports.conf
 
 make -C go isuports
 
