881ce52c58276583e4cf2bea1e155bcc3352c159
tosuke <13393900+tosuke@users.noreply.github.com>
2024-12-01 23:55:23 +0900

    mysql

--- deploy-local.sh
+++ deploy-local.sh
@@ -9,6 +9,7 @@ sudo systemctl daemon-reload
 
 envsubst '$GIT_REV' <conf/nginx.conf | sudo tee /etc/nginx/nginx.conf >/dev/null
 sudo cp conf/nginx-isuports.conf /etc/nginx/sites-available/isuports.conf
+envsubst '$GIT_REV' <conf/my.conf | sudo tee /etc/mysql/mysql.conf.d/isuports.conf >/dev/null
 
 make -C go isuports
 
