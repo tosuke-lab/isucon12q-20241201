bb05ac206d8ca1475cc8bc3b5ce247639155df8b
tosuke <13393900+tosuke@users.noreply.github.com>
2024-12-01 23:30:29 +0900

    alp

---
+++ alp-local.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -xe
+
+rev=${1:-HEAD}
+hash=$(git rev-parse --short "$rev")
+rule="-m '^api/player/competition/[\w-]+/ranking$,^/api/player/player/[\w-]+$,^/api/organizer/competition/[\w-]+$,^/api/organizer/competition/[\w-]+/score$,^/api/organizer/competition/[\w-]+/finish$,^/api/organizer/player/[\w-]+/disqualified$'"
+
+sudo alp ltsv --file /var/log/nginx/access."$hash".log $rule
