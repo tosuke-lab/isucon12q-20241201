90e8dde7ec6672396ad5296c42d8d1b3ee8f6ad5
tosuke <13393900+tosuke@users.noreply.github.com>
2024-12-02 19:32:24 +0900

    migrate tenant schema

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,9 @@
 DELETE FROM tenant WHERE id > 100;
 DELETE FROM visit_history WHERE created_at >= '1654041600';
 DELETE FROM first_visit WHERE created_at >= '1654041600';
-DELETE FROM billing_report;
+DELETE FROM billing_report WHERE created_at >= '1654041600';
+DELETE FROM competition WHERE created_at >= '1654041600';
+DELETE FROM player WHERE created_at >= '1654041600';
+DELETE FROM player_score WHERE created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
