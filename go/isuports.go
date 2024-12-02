b3541c15c79a67d7afa08ecd56631a016369a593
tosuke <13393900+tosuke@users.noreply.github.com>
2024-12-03 01:27:36 +0900

    log level

--- go/isuports.go
+++ go/isuports.go
@@ -120,8 +120,8 @@ func SetCacheControlPrivate(next echo.HandlerFunc) echo.HandlerFunc {
 // Run は cmd/isuports/main.go から呼ばれるエントリーポイントです
 func Run() {
 	e := echo.New()
-	e.Debug = true
-	e.Logger.SetLevel(log.DEBUG)
+	// e.Debug = true
+	e.Logger.SetLevel(log.ERROR)
 
 	var (
 		sqlLogger io.Closer
