2fd9b19b8b22d480863fdedc430bce23f18a9bf3
tosuke <13393900+tosuke@users.noreply.github.com>
2024-12-03 01:15:47 +0900

    deploy isu2

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 set -xe
 
 ssh isucon@isu01.i12q.tosuke.dev "cd webapp && git pull && ./deploy-local.sh" 2>&1 | sed -E "s/^/[isu01] /" &
+ssh isucon@isu02.i12q.tosuke.dev "cd webapp && git pull && ./deploy-local.sh" 2>&1 | sed -E "s/^/[isu02] /" &
 ssh isucon@isu03.i12q.tosuke.dev "cd webapp && git pull && ./deploy-local.sh" 2>&1 | sed -E "s/^/[isu03] /" &
 
 wait
