93fad1ed203123e694e28235cda129bb5dc66928
isucon <isucon@example.com>
2024-12-02 12:38:49 +0000

    new init

--- sql/admin/10_schema.sql
+++ sql/admin/10_schema.sql
@@ -3,6 +3,9 @@ USE `isuports`;
 DROP TABLE IF EXISTS `tenant`;
 DROP TABLE IF EXISTS `id_generator`;
 DROP TABLE IF EXISTS `visit_history`;
+DROP TABLE IF EXISTS `competition`;
+DROP TABLE IF EXISTS `player`;
+DROP TABLE IF EXISTS `player_score`;
 
 CREATE TABLE `tenant` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
@@ -28,4 +31,37 @@ CREATE TABLE `visit_history` (
   `created_at` BIGINT NOT NULL,
   `updated_at` BIGINT NOT NULL,
   INDEX `tenant_id_idx` (`tenant_id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+CREATE TABLE `competition` (
+	`id` VARCHAR(255) NOT NULL PRIMARY KEY,
+	`tenant_id` BIGINT NOT NULL,
+	`title` TEXT NOT NULL,
+	`finished_at` BIGINT NULL,
+	`created_at` BIGINT NOT NULL,
+	`updated_at` BIGINT NOT NULL,
+	INDEX `tenant_id_idx` (`tenant_id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+CREATE TABLE `player` (
+	`id` VARCHAR(255) NOT NULL PRIMARY KEY,
+	`tenant_id` BIGINT NOT NULL,
+	`display_name` TEXT NOT NULL,
+	`is_disqualified` BOOLEAN NOT NULL,
+	`created_at` BIGINT NOT NULL,
+	`updated_at` BIGINT NOT NULL,
+	INDEX `tenant_id_idx` (`tenant_id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+CREATE TABLE `player_score` (
+	`id` VARCHAR(255) NOT NULL PRIMARY KEY,
+	`tenant_id` BIGINT NOT NULL,
+	`player_id` VARCHAR(255) NOT NULL,
+	`competition_id` VARCHAR(255) NOT NULL,
+	`score` BIGINT NOT NULL,
+	`row_num` BIGINT NOT NULL,
+	`created_at` BIGINT NOT NULL,
+	`updated_at` BIGINT NOT NULL,
+	INDEX `tenant_id_idx` (`tenant_id`),
+	INDEX `created_at_idx` (`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
--- sql/admin/99_migration.sql
+++ sql/admin/99_migration.sql
@@ -2,9 +2,6 @@ USE `isuports`;
 
 DROP TABLE IF EXISTS `first_visit`;
 DROP TABLE IF EXISTS `billing_report`;
-DROP TABLE IF EXISTS `competition`;
-DROP TABLE IF EXISTS `player`;
-DROP TABLE IF EXISTS `player_score`;
 
 CREATE TABLE `first_visit` (
   `player_id` VARCHAR(255) NOT NULL,
@@ -32,36 +29,4 @@ CREATE TABLE `billing_report` (
 	`created_at` BIGINT NOT NULL,
 	INDEX `tenant_id_idx` (`tenant_id`),
 	UNIQUE KEY `tenant_competition` (`tenant_id`, `competition_id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
-
-CREATE TABLE `competition` (
-	`id` VARCHAR(255) NOT NULL PRIMARY KEY,
-	`tenant_id` BIGINT NOT NULL,
-	`title` TEXT NOT NULL,
-	`finished_at` BIGINT NULL,
-	`created_at` BIGINT NOT NULL,
-	`updated_at` BIGINT NOT NULL,
-	INDEX `tenant_id_idx` (`tenant_id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
-
-CREATE TABLE `player` (
-	`id` VARCHAR(255) NOT NULL PRIMARY KEY,
-	`tenant_id` BIGINT NOT NULL,
-	`display_name` TEXT NOT NULL,
-	`is_disqualified` BOOLEAN NOT NULL,
-	`created_at` BIGINT NOT NULL,
-	`updated_at` BIGINT NOT NULL,
-	INDEX `tenant_id_idx` (`tenant_id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
-
-CREATE TABLE `player_score` (
-	`id` VARCHAR(255) NOT NULL PRIMARY KEY,
-	`tenant_id` BIGINT NOT NULL,
-	`player_id` VARCHAR(255) NOT NULL,
-	`competition_id` VARCHAR(255) NOT NULL,
-	`score` BIGINT NOT NULL,
-	`row_num` BIGINT NOT NULL,
-	`created_at` BIGINT NOT NULL,
-	`updated_at` BIGINT NOT NULL,
-	INDEX `tenant_id_idx` (`tenant_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
