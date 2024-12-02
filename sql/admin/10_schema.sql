e6192cbf734a18f04e285c5f1f1ff3feef6448f8
tosuke <13393900+tosuke@users.noreply.github.com>
2024-12-02 23:41:16 +0900

    player_score (competition, player)

--- sql/admin/10_schema.sql
+++ sql/admin/10_schema.sql
@@ -6,6 +6,7 @@ DROP TABLE IF EXISTS `visit_history`;
 DROP TABLE IF EXISTS `competition`;
 DROP TABLE IF EXISTS `player`;
 DROP TABLE IF EXISTS `player_score`;
+DROP TABLE IF EXISTS `billing_report`;
 
 CREATE TABLE `tenant` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
@@ -63,5 +64,18 @@ CREATE TABLE `player_score` (
 	`created_at` BIGINT NOT NULL,
 	`updated_at` BIGINT NOT NULL,
 	INDEX `tenant_id_idx` (`tenant_id`),
-	INDEX `created_at_idx` (`created_at`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+CREATE TABLE `billing_report` (
+	`tenant_id` BIGINT UNSIGNED NOT NULL,
+	`competition_id` VARCHAR(255) NOT NULL,
+	`competition_title` TEXT NOT NULL,
+	`player_count` BIGINT NOT NULL,
+	`visitor_count` BIGINT NOT NULL,
+	`billing_player_yen` BIGINT NOT NULL,
+	`billing_visitor_yen` BIGINT NOT NULL,
+	`billing_yen` BIGINT NOT NULL,
+	`created_at` BIGINT NOT NULL,
+	INDEX `tenant_id_idx` (`tenant_id`),
+	UNIQUE KEY `tenant_competition` (`tenant_id`, `competition_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
--- sql/admin/99_migration.sql
+++ sql/admin/99_migration.sql
@@ -1,7 +1,6 @@
 USE `isuports`;
 
 DROP TABLE IF EXISTS `first_visit`;
-DROP TABLE IF EXISTS `billing_report`;
 
 CREATE TABLE `first_visit` (
   `player_id` VARCHAR(255) NOT NULL,
@@ -17,16 +16,6 @@ SELECT player_id, tenant_id, competition_id, MIN(created_at) AS created_at
 FROM visit_history
 GROUP BY player_id, tenant_id, competition_id;
 
-CREATE TABLE `billing_report` (
-	`tenant_id` BIGINT UNSIGNED NOT NULL,
-	`competition_id` VARCHAR(255) NOT NULL,
-	`competition_title` TEXT NOT NULL,
-	`player_count` BIGINT NOT NULL,
-	`visitor_count` BIGINT NOT NULL,
-	`billing_player_yen` BIGINT NOT NULL,
-	`billing_visitor_yen` BIGINT NOT NULL,
-	`billing_yen` BIGINT NOT NULL,
-	`created_at` BIGINT NOT NULL,
-	INDEX `tenant_id_idx` (`tenant_id`),
-	UNIQUE KEY `tenant_competition` (`tenant_id`, `competition_id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+ALTER TABLE player_score
+DROP INDEX competition_player_idx,
+ADD INDEX competition_player_idx (competition_id, player_id);
