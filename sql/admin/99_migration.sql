a8f44b8ccd45ba68bcab1b4ab4126dd0995def93
tosuke <13393900+tosuke@users.noreply.github.com>
2024-12-03 04:05:10 +0900

    player_rank

--- sql/admin/99_migration.sql
+++ sql/admin/99_migration.sql
@@ -16,6 +16,21 @@ SELECT player_id, tenant_id, competition_id, MIN(created_at) AS created_at
 FROM visit_history
 GROUP BY player_id, tenant_id, competition_id;
 
+DROP TABLE IF EXISTS `player_rank`;
+CREATE TABLE `player_rank` (
+	`player_id` VARCHAR(255) NOT NULL,
+	`competition_id` VARCHAR(255) NOT NULL,
+	`competition_title` TEXT NOT NULL,
+	`competition_created_at` BIGINT NOT NULL,
+	`score` BIGINT NOT NULL,
+	UNIQUE KEY `player_competition` (`player_id`, `competition_id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+INSERT INTO player_rank (player_id, competition_id, competition_title, competition_created_at, score)
+SELECT ps.player_id AS player_id, c.id AS competition_id, c.title AS competition_title, c.created_at AS competition_created_at, ps.score AS score
+FROM player_score AS ps
+INNER JOIN competition AS c ON c.id = ps.competition_id;
+
 ALTER TABLE player_score
 DROP INDEX competition_player_idx,
 ADD INDEX competition_player_idx (competition_id, player_id),
--- sql/init.sql
+++ sql/init.sql
@@ -5,5 +5,6 @@ DELETE FROM billing_report WHERE created_at >= '1654041600';
 DELETE FROM competition WHERE created_at >= '1654041600';
 DELETE FROM player WHERE created_at >= '1654041600';
 DELETE FROM player_score WHERE created_at >= '1654041600';
+DELETE FROM player_rank WHERE competition_created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
